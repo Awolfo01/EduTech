6b70de71b6a5167687687295d859e37bfc92b48d
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-05-08 23:13:57 -0400

    Agrega ModuloRepository.java en micro2/repository con manejo de bd

---
+++ src/main/java/com/edutech/plataforma_educativa/micro2cursos/repository/ModuloRepository.java
@@ -0,0 +1,7 @@
+package com.edutech.plataforma_educativa.micro2cursos.repository;
+
+import com.edutech.plataforma_educativa.micro2cursos.model.Modulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuloRepository extends JpaRepository<Modulo, Long> {
+}
