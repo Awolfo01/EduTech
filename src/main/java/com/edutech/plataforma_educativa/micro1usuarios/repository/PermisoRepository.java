60d78fa463510fa8ecb7e98c6face7fa8817989b
Felipe-aprendiendo <felia.hernandez@duocuc.cl>
2025-05-09 21:40:11 -0400

    Agrega permiso en model, service, controller, repository

---
+++ src/main/java/com/edutech/plataforma_educativa/micro1usuarios/repository/PermisoRepository.java
@@ -0,0 +1,8 @@
+package com.edutech.plataforma_educativa.micro1usuarios.repository;
+
+import com.edutech.plataforma_educativa.micro1usuarios.model.Permiso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermisoRepository extends JpaRepository<Permiso, Long> {
+    //bd 
+}
