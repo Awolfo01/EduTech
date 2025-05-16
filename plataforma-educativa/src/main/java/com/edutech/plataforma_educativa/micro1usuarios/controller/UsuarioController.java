7580d321d557c335103e69a29aa4327e031ec266
Catalina Leal Rodríguez <cat.lealr@duocuc.cl>
2025-05-15 22:56:36 -0400

    inplementación de boton de administrador

--- plataforma-educativa/src/main/java/com/edutech/plataforma_educativa/micro1usuarios/controller/UsuarioController.java
+++ plataforma-educativa/src/main/java/com/edutech/plataforma_educativa/micro1usuarios/controller/UsuarioController.java
@@ -1,7 +1,5 @@
 package com.edutech.plataforma_educativa.micro1usuarios.controller;
 
-
-
 import org.springframework.web.bind.annotation.*;
 import com.edutech.plataforma_educativa.micro1usuarios.model.Usuario;
 import com.edutech.plataforma_educativa.micro1usuarios.service.UsuarioService;
--- plataforma-educativa/src/main/java/com/edutech/plataforma_educativa/micro1usuarios/controller/VistaController.java
+++ plataforma-educativa/src/main/java/com/edutech/plataforma_educativa/micro1usuarios/controller/VistaController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VistaController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        return "home";  // Nombre de la vista (archivo HTML en resources/templates)
-    }
+    // @GetMapping("/")
+    // public String home(Model model) {
+    //     return "home";  // Nombre de la vista (archivo HTML en resources/templates)
+    // }
 
         @GetMapping("/aula")
     public String aula(Model model) {
